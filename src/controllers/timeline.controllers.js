7941ad8355e832887527ced232b4ea3c619a6894
devlucaspg <110506094+devlucaspg@users.noreply.github.com>
2023-01-05 07:11:03 -0400

    fix: route timeline

--- src/controllers/timeline.controllers.js
+++ src/controllers/timeline.controllers.js
@@ -3,7 +3,12 @@ import { connectionDb } from "../database/database";
 export async function getPosts(req, res) {
   try {
     const posts = await connectionDb.query(
-      "SELECT * FROM posts ORDER BY id DESC LIMIT 20"
+      `
+      SELECT posts.*, users.username, users.picture, users.id AS "userId"
+      FROM posts 
+      JOIN users ON users.id = posts."userId" 
+      ORDER BY id DESC LIMIT 20
+      `
     );
     res.send(posts.rows);
   } catch (error) {
