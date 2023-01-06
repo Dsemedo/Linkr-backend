c1694365a16aee30b1356e272da5fa3537e16b32
Douglas Semedo <dsemedoo@gmail.com>
2023-01-06 14:10:16 -0300

    Share dataUser to timeline

--- src/routes/linkr.routes.js
+++ src/routes/linkr.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
-import { linkrController } from "../controllers/linkr.controller.js";
+import { linkrController, getPosts } from "../controllers/linkr.controller.js";
 import { linkrMiddleware } from "../middlewares/linkr.middleware.js";
 export const linkrRoute = Router();
 
 linkrRoute.post("/timeline", linkrMiddleware, linkrController);
+
+linkrRoute.get("/timeline", getPosts);
