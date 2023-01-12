e94d40a00945511d78c16fd52ef8439c5c721384
gabrielgusso <gabriel.gusso@hotmail.com>
2023-01-12 13:00:48 -0300

    Fix: user/:id route

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,7 +5,7 @@ export async function userController(req, res) {
   try {
     const posts = await connectionDb.query(
       `
-        SELECT p."userId", p.id, p.description, p.link, u.username, u.picture, COUNT(l."idPost") as likes 
+      SELECT p.id, p.description, p.link, p."userId", u.username, u.picture, COUNT(l."idPost") as likes, json_agg(l.username) as "usersWhoLiked"
       FROM posts p
       JOIN users u
       ON p."userId" = u.id
