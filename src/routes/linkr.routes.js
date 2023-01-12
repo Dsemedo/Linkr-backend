d61f358f608c1c9580d6d0c84e6772fc8ee43231
gabrielgusso <gabriel.gusso@hotmail.com>
2023-01-12 19:51:28 -0300

    Feat: show only follow posts

--- src/routes/linkr.routes.js
+++ src/routes/linkr.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { linkrController, getPosts, linkrDeleteController, linkrPatchController } from "../controllers/linkr.controller.js";
-import { linkrMiddleware, deleteMiddleware } from "../middlewares/linkr.middleware.js";
+import { linkrMiddleware, deleteMiddleware, tokenMiddleware } from "../middlewares/linkr.middleware.js";
 export const linkrRoute = Router();
 
 linkrRoute.post("/timeline", linkrMiddleware, linkrController);
 
-linkrRoute.get("/timeline", getPosts);
+linkrRoute.get("/timeline", tokenMiddleware, getPosts);
 
 linkrRoute.delete("/timeline/:id", deleteMiddleware , linkrDeleteController);
 
